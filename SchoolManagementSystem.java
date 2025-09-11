2972c91f5b4f2ea3d27c78e900c49f8309bd5146
ubddprasanna <ubddprasannapro@gmail.com>
2025-09-11 12:54:28 +0530

    Add initial implementation of School Management System

---
+++ SchoolManagementSystem.java
@@ -0,0 +1,17 @@
+public class SchoolManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to CyberCrewz Academy \nSchool Management System V1 \nCode Name River ");
+
+        while (true) {
+            System.out.println("--- Main Menu ---");
+            System.out.println("""
+                    1. Student Management
+                    2. Teacher Management
+                    3. Library Management
+                    4. Reports
+                    0. Exit""");
+
+        }
+    }
+}
