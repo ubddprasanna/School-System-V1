66757279dc11e0d14a8da46c19b8ce695f2dc241
ubddprasanna <ubddprasannapro@gmail.com>
2025-09-11 13:29:58 +0530

    Add settings for Java formatter and improve menu output in School Management System

--- SchoolManagementSystem.java
+++ SchoolManagementSystem.java
@@ -1,16 +1,36 @@
+
+import java.util.Scanner;
+
 public class SchoolManagementSystem {
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to CyberCrewz Academy \nSchool Management System V1 \nCode Name River ");
 
+        menu();
+
+    }
+
+    public static void menu() {
+        Scanner input = new Scanner(System.in);
+
         while (true) {
-            System.out.println("--- Main Menu ---");
+            System.out.println("\n--- Main Menu ---");
             System.out.println("""
                     1. Student Management
                     2. Teacher Management
                     3. Library Management
                     4. Reports
-                    0. Exit""");
+                    0. Exit
+                    """);
+
+            System.err.print("Enter your choice: ");
+            int userInput = input.nextInt();
+
+            if (userInput == 0) {
+                System.out.println("Thank you for using the School Management System. Goodbye!");
+                break;
+            };
 
         }
     }
